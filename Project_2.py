24652d1fa9564c48098f1364a3275f45c5bd0502
Mnanth <mmathanaraj@torontomu.ca>
2024-11-07 13:37:43 -0500

    Project 2

---
+++ Project_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  7 12:18:29 2024
+
+@author: mithu
+"""
+
+import tensorflow as tf
+import pandas as pd
+import numpy as nm
+
+from tensorflow.keras import datasets, layers, models
+
+
