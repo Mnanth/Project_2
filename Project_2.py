f3db6fd93e71600c321be6ce6ca5465f96c18783
Mnanth <mmathanaraj@torontomu.ca>
2024-11-14 02:41:14 -0500

    Update Project_2.py

--- Project_2.py
+++ Project_2.py
@@ -15,6 +15,7 @@ from pathlib import Path
 
 from tensorflow import keras
 from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image_dataset_from_directory
@@ -51,3 +52,20 @@ validation_data = image_dataset_from_directory(
     label_mode = 'categorical',
     image_size = (128,128),
     batch_size = 32)
+
+#Step 2: Neural Network Architecture Design
+model = Sequential()
+
+model.add(Conv2D (32, (3,3), activation = 'relu', input_shape=(500,500,3)))
+
+model.add(MaxPooling2D(pool_size = (2,2)))
+
+model.add(Flatten())
+
+model.add (Dense(128, activation = 'relu'))
+
+model.add(Dense(3, activation = 'softmax'))
+
+model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+model.summary()
